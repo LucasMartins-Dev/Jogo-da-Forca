4a828a9dc4d048097d259efcb0323ffbd983dc88
LucasMartins-Dev <lucasmartinsms.dev@gmail.com>
2022-11-23 22:53:16 -0300

    contador de letras corretas

--- src/components/appp.js
+++ src/components/appp.js
@@ -3,12 +3,32 @@ import React from "react";
 import Letras from './letras.js';
 import Jogo from './jogo.js';
 import Chute from './chute.js';
+import palavras from "../palavras.js";
 
 export default function Appp(){
+  let arrpalavra;
+  const[escolherpalavra,setescolherpalavra] = React.useState('')
+    const[palavrajogo,setpalavrajogo] = React.useState('')
+    function Iniciarjogo(){
+      const palavraescolhida = Math.floor(Math.random()*palavras.length);
+      setpalavrajogo (palavras[palavraescolhida])
+      setescolherpalavra(<div className="button gray"> Escolher Palavra</div>)
+      
+      Habilitarletra()
+    
+  }
 
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    const[selecionados,setselecionados]=React.useState([])
+    console.log(selecionados)
+    arrpalavra = palavrajogo.split('')
+    console.log(palavrajogo)
+    console.log(arrpalavra)
+  
     const[letra,setletra]=React.useState('letra lgray')
     const[tentar,settentar]=React.useState(false)
+    const[cont,setcont]=React.useState(0)
+    console.log(cont)
     function Habilitarletra(){
             setletra('letra')
             settentar(true)
@@ -17,9 +37,29 @@ export default function Appp(){
 
     return(
     <div className="App">
-      <Jogo funct = {Habilitarletra}/>
+      <Jogo funct = {Habilitarletra}
+      functi={Iniciarjogo}
+      escolherpalavra ={escolherpalavra}
+      setescolherpalavra={setescolherpalavra}
+      palavrajogo = {palavrajogo}
+      setpalavrajogo = {setpalavrajogo}
+      palavras={palavras}
+      />
       <div className="letras">
-        {alfabeto.map((a)=><Letras a ={a}letra = {letra} setletra={setletra}tentativa = {tentar} key={a}/>
+        {alfabeto.map((a)=>
+        <Letras 
+        selecionados={selecionados} 
+        a ={a}
+        letra = {letra} 
+        setletra={setletra}
+        tentativa = {tentar} 
+        key={a}
+        arrpalavra={arrpalavra}
+        setselecionados={setselecionados}
+        cont = {cont}
+        setcont= {setcont}
+        
+        />
            )}
            </div>
       <Chute/>
--- src/components/letras.js
+++ src/components/letras.js
@@ -3,30 +3,33 @@ import React from "react";
 
 export default function Letras(props){
 
-let count = 0;
-
-const[tentar,settentar]=React.useState('')
 
 
-function Tentativa(){
-    
-    if(props.tentativa === true){
-        settentar('lgray')
-        console.log('true')
-        count ++
-        console.log(count)
-           
-    }else{
-        
-        console.log('false')
+function clicouletra(){
+    const estaNaLista = props.selecionados.includes(props.a)
+    if(!estaNaLista){
+        let resul = 0;
+        const aux = [...props.selecionados,props.a]
+        props.setselecionados(aux)
+        for(let i=0;i<props.arrpalavra.length;i++){
+            
+            if(aux[aux.length -1]=== props.arrpalavra[i]){
+                console.log(props.arrpalavra[i])
+               console.log(aux[aux.length -1])
+                resul ++
+                console.log(resul)
+                props.setcont(props.cont + resul)
+            }
+            
+        }
+       
     }
-    
-}
+  }
 
 
 return(
     
-    <div onClick = {Tentativa} className={`${props.letra} ${tentar}`}>{props.a}</div>
+    <div  onClick={clicouletra} className={`${props.letra} ${ props.selecionados.includes(props.a) ? "lgray" : ""} `}>{props.a}</div>
      
 )
 
